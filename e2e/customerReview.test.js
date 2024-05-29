c1fa9387d2de2a0d2410f0e633408d0265cc8d21
farhanScripts <farhan.dev06@gmail.com>
2024-05-29 09:29:15 +0700

    Complete customer review test

--- e2e/customerReview.test.js
+++ e2e/customerReview.test.js
@@ -1,11 +1,26 @@
 /* eslint-disable no-undef */
+
 Feature('Customer Review Restaurant');
 
 Before(({ I }) => {
   I.amOnPage('/');
 });
 
-Scenario('Adding 1 reviews to one restaurant', async () => {
+Scenario('Adding 1 reviews to one restaurant', async ({ I }) => {
   I.seeElement('.resto_name a');
   I.click(locate('.resto_name a').first());
+
+  I.fillField('#Name', 'Farhan Putra');
+  I.fillField('#review', 'Makanannya enak!');
+
+  I.click('button[type="submit"]');
+
+  I.executeScript(() => {
+    window.location.reload();
+  });
+
+  I.waitForNavigation();
+
+  I.see('Farhan Putra');
+  I.see('Makanannya enak!');
 });
